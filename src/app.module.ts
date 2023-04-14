df4a13687545a9f650dd1f553e7ccc715ae81c4f
jadz200 <jadzarzour3@gmail.com>
2023-04-14 19:49:48 +0300

    fixing swagger on vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import { join } from 'path';
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'swagger-static'),
-      serveRoot: process.env.NODE_ENV === 'development' ? '/' : '/swagger',
+      serveRoot: process.env.NODE_ENV === 'development' ? '/' : '/api',
     }),
     ConfigModule.forRoot({
       isGlobal: true,
