c0ea9f0db0005918fb15770779e45ec3c67ab5af
jadz200 <jadzarzour3@gmail.com>
2023-04-20 09:35:18 +0300

    fixing cors issue vercel

--- src/appointment/dto/getPaymentInfo.dto.ts
+++ src/appointment/dto/getPaymentInfo.dto.ts
@@ -1,11 +1,10 @@
-import { ObjectId } from 'mongoose';
 import { paymentInfoDto } from './createAppointment.dto';
 
 export class GetpaymentInfoDto {
   _id: string;
-  patient_id?: string;
+  patient_id: string;
   fullName?: string;
   email?: string;
   image?: string;
-  paymentInfo?: paymentInfoDto;
+  paymentInfo: paymentInfoDto;
 }
