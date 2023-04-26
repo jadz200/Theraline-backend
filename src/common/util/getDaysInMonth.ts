5eb4661a9d795a28c500f43929e36c277572e17f
jadz200 <jadzarzour3@gmail.com>
2023-04-26 12:26:33 +0300

    fix getDaysInMonth

--- src/common/util/getDaysInMonth.ts
+++ src/common/util/getDaysInMonth.ts
@@ -1,9 +1,9 @@
-export function getDaysInMonth(year: number, month: number): number[] {
-  const daysInMonth: number[] = [];
+export function getDaysInMonth(year: number, month: number): string[] {
+  const daysInMonth: string[] = [];
   const numDays = new Date(year, month, 0).getDate();
 
   for (let i = 1; i <= numDays; i += 1) {
-    daysInMonth.push(i);
+    daysInMonth.push(`Day ${i}`);
   }
 
   return daysInMonth;
